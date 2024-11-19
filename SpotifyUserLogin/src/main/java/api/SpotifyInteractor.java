b10a4ae035e18792b427eb3d31c5e04de746cd4b
Jack Wang <jaack.wang@mail.utoronto.ca>
2024-11-19 16:06:26 -0500

    Testing SpotifyInteractor

--- SpotifyUserLogin/src/main/java/api/SpotifyInteractor.java
+++ SpotifyUserLogin/src/main/java/api/SpotifyInteractor.java
@@ -1,6 +1,7 @@
 package api;
 
 import org.apache.hc.core5.http.ParseException;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.model_objects.credentials.AuthorizationCodeCredentials;
@@ -194,6 +195,31 @@ public class SpotifyInteractor extends AbstractSpotifyInteractor{
     public static void main(String[] args) {
         SpotifyInteractor interactor = new SpotifyInteractor();
         interactor.login();
-        interactor.getArtist("6RQ9kYbHisp1UUbnfwHNeU");
+
+        System.out.println("Get Artist test:");
+        System.out.println(interactor.getArtist("6RQ9kYbHisp1UUbnfwHNeU").getString("name"));
+
+        System.out.println("Get Playlist items test:");
+        JSONArray playlistTracks = interactor.getPlaylistItems("3NTWh52BO994lvgv3EJLyn", 1, 0).getJSONArray("items");
+        JSONObject firstTrack = playlistTracks.getJSONObject(0).getJSONObject("track");
+        System.out.println(firstTrack.getString("name"));
+
+        System.out.println("Get Current User Playlists test:");
+        JSONArray currentUserPlaylists = interactor.getCurrentUserPlaylists(1, 0).getJSONArray("items");
+        JSONObject currentUserPlaylist = currentUserPlaylists.getJSONObject(0);
+        System.out.println(currentUserPlaylist.getString("name"));
+
+        System.out.println("Get User Playlist test:");
+        JSONArray userPlaylists = interactor.getUserPlaylists("jjunevo", 1, 0).getJSONArray("items");
+        JSONObject userPlaylist = userPlaylists.getJSONObject(0);
+        System.out.println(userPlaylist.getString("name"));
+
+        System.out.println("Get Current User Profile test:");
+        JSONObject currentUserProfile = interactor.getCurrentUserProfile();
+        System.out.println(currentUserProfile.getString("displayName"));
+
+        System.out.println("Get User Profile test:");
+        JSONObject userProfile = interactor.getUserProfile("jjunevo");
+        System.out.println(userProfile.getString("displayName"));
     }
 }
