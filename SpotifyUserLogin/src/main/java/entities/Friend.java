eea2e4eee0d44a57b9c8dbee1281b9fff7c27544
Jack Wang <jaack.wang@mail.utoronto.ca>
2024-11-26 15:20:25 -0500

    Completed a basic Friend class that includes the data from the "owner" key in the getCurrentUserPlaylists request

--- SpotifyUserLogin/src/main/java/entities/Friend.java
+++ SpotifyUserLogin/src/main/java/entities/Friend.java
@@ -1,4 +1,28 @@
 package entities;
 
+import org.json.JSONObject;
+
 public class Friend {
+
+    private final JSONObject miscFriendData;
+    private final String id;
+    private final String name;
+
+    public Friend(JSONObject friendData) {
+        miscFriendData = friendData;
+        id = friendData.getString("id");
+        name = friendData.getString("name");
+    }
+
+    public JSONObject getMiscFriendData() {
+        return miscFriendData;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
