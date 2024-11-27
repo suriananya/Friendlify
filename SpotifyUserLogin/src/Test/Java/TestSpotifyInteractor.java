604bfc473639a5a0132f2403bfb51295b5c44fbc
Jack Wang <jaack.wang@mail.utoronto.ca>
2024-11-26 21:09:34 -0500

    Removed JUnit from TestSpotifyInteractor because it does not allow user interaction. This makes it impossible to attain an access token required to test API calls

--- SpotifyUserLogin/src/Test/Java/TestSpotifyInteractor.java
+++ SpotifyUserLogin/src/Test/Java/TestSpotifyInteractor.java
@@ -1,24 +1,18 @@
 import api.SpotifyInteractor;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.junit.BeforeClass;
-import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestSpotifyInteractor {
     private static final SpotifyInteractor interactor = new SpotifyInteractor();
 
-    @BeforeClass
-    public static void init() {
-        interactor.login();
+    public static void getArtist() {
+        String artistName = interactor.getArtist("6RQ9kYbHisp1UUbnfwHNeU").getString("name");
+        assertEquals("Feint", artistName);
     }
 
-    @Test
-    public void getArtist() {
-        System.out.println(interactor.getArtist("6RQ9kYbHisp1UUbnfwHNeU").getString("name"));
-    }
-
-    @Test
-    public void getPlaylistItems() {
+    public static void getPlaylistItems() {
         JSONArray playlistTracks = interactor
                 .getPlaylistItems("3NTWh52BO994lvgv3EJLyn", 1, 0)
                 .getJSONArray("items");
@@ -28,8 +22,7 @@ public class TestSpotifyInteractor {
         System.out.println(firstTrack.getString("name"));
     }
 
-    @Test
-    public void getCurrentUserPlaylist() {
+    public static void getCurrentUserPlaylist() {
         JSONArray currentUserPlaylists = interactor
                 .getCurrentUserPlaylists(1, 0)
                 .getJSONArray("items");
@@ -39,8 +32,7 @@ public class TestSpotifyInteractor {
         System.out.println(currentUserPlaylist.getString("name"));
     }
 
-    @Test
-    public void getUserPlaylists() {
+    public static void getUserPlaylists() {
         JSONArray userPlaylists = interactor
                 .getUserPlaylists("jjunevo", 1, 0)
                 .getJSONArray("items");
@@ -50,19 +42,30 @@ public class TestSpotifyInteractor {
         System.out.println(userPlaylist.getString("name"));
     }
 
-    @Test
-    public void getCurrentUserProfile() {
+    public static void getCurrentUserProfile() {
         JSONObject currentUserProfile = interactor
                 .getCurrentUserProfile();
 
         System.out.println(currentUserProfile.getString("displayName"));
     }
 
-    @Test
-    public void getUserProfile() {
+    public static void getUserProfile() {
         JSONObject userProfile = interactor
                 .getUserProfile("jjunevo");
 
         System.out.println(userProfile.getString("displayName"));
+    }
+
+    public static void main(String[] args) {
+        interactor.login();
+
+        getArtist();
+        getPlaylistItems();
+
+        getCurrentUserPlaylist();
+        getUserPlaylists();
+
+        getCurrentUserProfile();
+        getUserProfile();
     }
 }
