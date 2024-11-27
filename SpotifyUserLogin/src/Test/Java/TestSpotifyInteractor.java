c5fcb6ad22a4a5f4270c5d77449070713e32c031
Jack Wang <jaack.wang@mail.utoronto.ca>
2024-11-26 20:55:23 -0500

    Split main test into numerous unit tests

--- SpotifyUserLogin/src/Test/Java/TestSpotifyInteractor.java
+++ SpotifyUserLogin/src/Test/Java/TestSpotifyInteractor.java
@@ -1,36 +1,68 @@
 import api.SpotifyInteractor;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class TestSpotifyInteractor {
-    public static void main(String[] args) {
-        SpotifyInteractor interactor = new SpotifyInteractor();
+    private static final SpotifyInteractor interactor = new SpotifyInteractor();
+
+    @BeforeClass
+    public static void init() {
         interactor.login();
+    }
 
-        System.out.println("Get Artist test:");
+    @Test
+    public void getArtist() {
         System.out.println(interactor.getArtist("6RQ9kYbHisp1UUbnfwHNeU").getString("name"));
+    }
 
-        System.out.println("Get Playlist items test:");
-        JSONArray playlistTracks = interactor.getPlaylistItems("3NTWh52BO994lvgv3EJLyn", 1, 0).getJSONArray("items");
+    @Test
+    public void getPlaylistItems() {
+        JSONArray playlistTracks = interactor
+                .getPlaylistItems("3NTWh52BO994lvgv3EJLyn", 1, 0)
+                .getJSONArray("items");
+
         JSONObject firstTrack = playlistTracks.getJSONObject(0).getJSONObject("track");
+
         System.out.println(firstTrack.getString("name"));
+    }
 
-        System.out.println("Get Current User Playlists test:");
-        JSONArray currentUserPlaylists = interactor.getCurrentUserPlaylists(1, 0).getJSONArray("items");
+    @Test
+    public void getCurrentUserPlaylist() {
+        JSONArray currentUserPlaylists = interactor
+                .getCurrentUserPlaylists(1, 0)
+                .getJSONArray("items");
+
         JSONObject currentUserPlaylist = currentUserPlaylists.getJSONObject(0);
+
         System.out.println(currentUserPlaylist.getString("name"));
+    }
 
-        System.out.println("Get User Playlist test:");
-        JSONArray userPlaylists = interactor.getUserPlaylists("jjunevo", 1, 0).getJSONArray("items");
+    @Test
+    public void getUserPlaylists() {
+        JSONArray userPlaylists = interactor
+                .getUserPlaylists("jjunevo", 1, 0)
+                .getJSONArray("items");
+
         JSONObject userPlaylist = userPlaylists.getJSONObject(0);
+
         System.out.println(userPlaylist.getString("name"));
+    }
 
-        System.out.println("Get Current User Profile test:");
-        JSONObject currentUserProfile = interactor.getCurrentUserProfile();
+    @Test
+    public void getCurrentUserProfile() {
+        JSONObject currentUserProfile = interactor
+                .getCurrentUserProfile();
+
         System.out.println(currentUserProfile.getString("displayName"));
+    }
 
-        System.out.println("Get User Profile test:");
-        JSONObject userProfile = interactor.getUserProfile("jjunevo");
+    @Test
+    public void getUserProfile() {
+        JSONObject userProfile = interactor
+                .getUserProfile("jjunevo");
+
         System.out.println(userProfile.getString("displayName"));
     }
 }
