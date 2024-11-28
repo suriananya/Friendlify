99d2fd5592fe60986b4f6d6266e8b96f2267a0a3
Jack Wang <jaack.wang@mail.utoronto.ca>
2024-11-27 21:40:18 -0500

    Split larger, main test into smaller unit tests

--- SpotifyUserLogin/src/Test/Java/TestApi/TestSpotifyInteractor.java
+++ SpotifyUserLogin/src/Test/Java/TestApi/TestSpotifyInteractor.java
@@ -1,36 +1,72 @@
+package TestApi;
+
 import api.SpotifyInteractor;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestSpotifyInteractor {
+    private static final SpotifyInteractor interactor = new SpotifyInteractor();
+
+    public static void getArtist() {
+        String artistName = interactor.getArtist("6RQ9kYbHisp1UUbnfwHNeU").getString("name");
+        assertEquals("Feint", artistName);
+    }
+
+    public static void getPlaylistItems() {
+        JSONArray playlistTracks = interactor
+                .getPlaylistItems("3NTWh52BO994lvgv3EJLyn", 1, 0)
+                .getJSONArray("items");
+
+        JSONObject firstTrack = playlistTracks.getJSONObject(0).getJSONObject("track");
+        assertEquals("Time", firstTrack.getString("name"));
+    }
+
+    public static void getCurrentUserPlaylist() {
+        JSONArray currentUserPlaylists = interactor
+                .getCurrentUserPlaylists(1, 0)
+                .getJSONArray("items");
+
+        JSONObject currentUserPlaylist = currentUserPlaylists.getJSONObject(0);
+
+        System.out.print("Check if this is the name of your one of your playlists: ");
+        System.out.println(currentUserPlaylist.getString("name"));
+    }
+
+    public static void getUserPlaylists() {
+        JSONArray userPlaylists = interactor
+                .getUserPlaylists("jjunevo", 1, 0)
+                .getJSONArray("items");
+
+        JSONObject userPlaylist = userPlaylists.getJSONObject(0);
+        assertEquals("sep13th", userPlaylist.getString("name"));
+    }
+
+    public static void getCurrentUserProfile() {
+        JSONObject currentUserProfile = interactor
+                .getCurrentUserProfile();
+        System.out.print("Check if this is your Spotify username: ");
+        System.out.println(currentUserProfile.getString("displayName"));
+    }
+
+    public static void getUserProfile() {
+        JSONObject userProfile = interactor
+                .getUserProfile("jjunevo");
+
+        assertEquals("juney", userProfile.getString("displayName"));
+    }
+
     public static void main(String[] args) {
-        SpotifyInteractor interactor = new SpotifyInteractor();
         interactor.login();
 
-        System.out.println("Get Artist test:");
-        System.out.println(interactor.getArtist("6RQ9kYbHisp1UUbnfwHNeU").getString("name"));
+        getArtist();
+        getPlaylistItems();
 
-        System.out.println("Get Playlist items test:");
-        JSONArray playlistTracks = interactor.getPlaylistItems("3NTWh52BO994lvgv3EJLyn", 1, 0).getJSONArray("items");
-        JSONObject firstTrack = playlistTracks.getJSONObject(0).getJSONObject("track");
-        System.out.println(firstTrack.getString("name"));
+        getCurrentUserPlaylist();
+        getUserPlaylists();
 
-        System.out.println("Get Current User Playlists test:");
-        JSONArray currentUserPlaylists = interactor.getCurrentUserPlaylists(1, 0).getJSONArray("items");
-        JSONObject currentUserPlaylist = currentUserPlaylists.getJSONObject(0);
-        System.out.println(currentUserPlaylist.getString("name"));
-
-        System.out.println("Get User Playlist test:");
-        JSONArray userPlaylists = interactor.getUserPlaylists("jjunevo", 1, 0).getJSONArray("items");
-        JSONObject userPlaylist = userPlaylists.getJSONObject(0);
-        System.out.println(userPlaylist.getString("name"));
-
-        System.out.println("Get Current User Profile test:");
-        JSONObject currentUserProfile = interactor.getCurrentUserProfile();
-        System.out.println(currentUserProfile.getString("displayName"));
-
-        System.out.println("Get User Profile test:");
-        JSONObject userProfile = interactor.getUserProfile("jjunevo");
-        System.out.println(userProfile.getString("displayName"));
+        getCurrentUserProfile();
+        getUserProfile();
     }
 }
