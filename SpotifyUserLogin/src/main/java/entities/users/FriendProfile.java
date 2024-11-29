1b4ed66e8269f3bac268e3cd384ac1bbb25cb83c
Jack Wang <jaack.wang@mail.utoronto.ca>
2024-11-28 22:07:42 -0500

    Implemented baseline FriendProfile code

--- SpotifyUserLogin/src/main/java/entities/users/FriendProfile.java
+++ SpotifyUserLogin/src/main/java/entities/users/FriendProfile.java
@@ -1,4 +1,27 @@
 package entities.users;
 
-public class FriendProfile {
+import api.SpotifyInteractor;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class FriendProfile extends AbstractUserProfile{
+    public FriendProfile(String userId, List<String> genres, List<String> artists) {
+        super(userId, genres, artists);
+    }
+
+    public FriendProfile(SpotifyInteractor interactor, String id) {
+        super(interactor);
+        this.userID = id;
+    }
+
+    @Override
+    JSONObject getUserPlaylistsJSON(int limit, int offset) {
+        return null;
+    }
+
+    @Override
+    JSONObject getUserProfileJSON() {
+        return null;
+    }
 }
