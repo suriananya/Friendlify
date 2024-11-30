1a70954d9b268882b81a6cbb528355c9d6a82a66
Jack Wang <jaack.wang@mail.utoronto.ca>
2024-11-30 18:20:51 -0500

    Removed debug print line

--- SpotifyUserLogin/src/main/java/entities/users/Utility.java
+++ SpotifyUserLogin/src/main/java/entities/users/Utility.java
@@ -7,7 +7,6 @@ public class Utility {
         JSONArray sanitized = new JSONArray();
         for (int i = 0; i < unsanitized.length(); i++) {
             if (!unsanitized.isNull(i)) {
-                System.out.println(unsanitized.getJSONObject(i).toString());
                 sanitized.put(unsanitized.getJSONObject(i));
             }
         }
