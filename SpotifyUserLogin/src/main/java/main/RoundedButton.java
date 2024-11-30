7b2e09bee675f9b4381ac46f44a22f4e000626b3
juneylvo <jjune.lo@gmail.com>
2024-11-30 15:14:50 -0500

    Added rounded button class for styling and main functioning

---
+++ SpotifyUserLogin/src/main/java/main/RoundedButton.java
@@ -0,0 +1,40 @@
+package main;
+
+import javax.swing.JButton;
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+
+public class RoundedButton extends JButton {
+    public RoundedButton(String text) {
+        super(text);
+        setFocusPainted(false);
+        setContentAreaFilled(false);
+        setOpaque(false);
+        setBackground(Color.BLACK); // Black background
+        setForeground(Color.WHITE); // White text
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // Set button background color to black
+        g2.setColor(getBackground());
+        g2.fill(new RoundRectangle2D.Float(0, 0, getWidth(), getHeight(), 30, 30)); // Rounded corners
+
+        // Draw the button text
+        g2.setColor(getForeground()); // Ensure text is white
+        FontMetrics fm = g2.getFontMetrics();
+        int textWidth = fm.stringWidth(getText());
+        int textHeight = fm.getAscent();
+        g2.drawString(getText(), (getWidth() - textWidth) / 2, (getHeight() + textHeight) / 2 - 3);
+
+        g2.dispose();
+    }
+
+    @Override
+    protected void paintBorder(Graphics g) {
+        // Do not paint any border to remove the white outline
+    }
+}
