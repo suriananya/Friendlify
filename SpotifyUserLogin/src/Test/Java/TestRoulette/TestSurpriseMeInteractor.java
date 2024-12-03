ace612462d745b3843fdced3ee69efcdac4cf1db
suriananya <suriananya19@gmail.com>
2024-12-03 02:48:41 -0500

    Creating instances of necessary objects and initialising SurpriseMeInteractor

--- SpotifyUserLogin/src/Test/Java/TestRoulette/TestSurpriseMeInteractor.java
+++ SpotifyUserLogin/src/Test/Java/TestRoulette/TestSurpriseMeInteractor.java
@@ -1,4 +1,10 @@
 package TestRoulette;
 
+import useCase.Roulette.SurpriseMeInteractor;
+
 public class TestSurpriseMeInteractor {
+
+    private final MockSpotifyInteractor mockSpotifyInteractor = new MockSpotifyInteractor();
+    private final MockUserProfile mockUserProfile = new MockUserProfile(mockSpotifyInteractor);
+    private final SurpriseMeInteractor surpriseMeInteractor = new SurpriseMeInteractor(mockSpotifyInteractor, mockUserProfile);
 }
