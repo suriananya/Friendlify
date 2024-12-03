2b0108d7366099c165e4468a4cd94b0507d8398f
suriananya <suriananya19@gmail.com>
2024-12-03 03:11:43 -0500

    Adjusted MockUserProfile to be able to setMockFriends to test the SurpriseMeInteractor better

--- SpotifyUserLogin/src/Test/Java/TestRoulette/MockUserProfile.java
+++ SpotifyUserLogin/src/Test/Java/TestRoulette/MockUserProfile.java
@@ -6,13 +6,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MockUserProfile extends UserProfile {
+    private List<String> mockFriends;
 
     public MockUserProfile(SpotifyInteractor interactor) {
         super(interactor);
     }
 
+    public void setMockFriends(List<String> mockFriends) {
+        this.mockFriends = mockFriends;
+    }
+
     @Override
     public List<String> getFriendsList(String type) {
-        return Arrays.asList("friend1", "friend2", "friend3", "friend4");
+        return mockFriends != null ? mockFriends : Arrays.asList("friend1", "friend2", "friend3", "friend4");
     }
 }
