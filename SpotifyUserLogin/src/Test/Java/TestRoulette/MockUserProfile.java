c1b8f76495d01f36a42c5d53d92ee04c6312f6b1
suriananya <suriananya19@gmail.com>
2024-12-03 02:37:53 -0500

    Created the mock data and imported necessary Array and List classes

--- SpotifyUserLogin/src/Test/Java/TestRoulette/MockUserProfile.java
+++ SpotifyUserLogin/src/Test/Java/TestRoulette/MockUserProfile.java
@@ -1,4 +1,13 @@
 package TestRoulette;
 
-public class MockUserProfile {
+import entities.users.UserProfile;
+import java.util.Arrays;
+import java.util.List;
+
+public class MockUserProfile extends UserProfile {
+
+    @Override
+    public List<String> getFriendsList(String type) {
+        return Arrays.asList("friend1", "friend2", "friend3", "friend4");
+    }
 }
