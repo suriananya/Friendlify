515086dc478a498d22cd10135b1329d7abc9f90b
suriananya <suriananya19@gmail.com>
2024-12-03 02:43:43 -0500

    Added a constructor that calls to parent class

--- SpotifyUserLogin/src/Test/Java/TestRoulette/MockUserProfile.java
+++ SpotifyUserLogin/src/Test/Java/TestRoulette/MockUserProfile.java
@@ -1,10 +1,15 @@
 package TestRoulette;
 
+import api.SpotifyInteractor;
 import entities.users.UserProfile;
 import java.util.Arrays;
 import java.util.List;
 
 public class MockUserProfile extends UserProfile {
+
+    public MockUserProfile(SpotifyInteractor interactor) {
+        super(interactor);
+    }
 
     @Override
     public List<String> getFriendsList(String type) {
