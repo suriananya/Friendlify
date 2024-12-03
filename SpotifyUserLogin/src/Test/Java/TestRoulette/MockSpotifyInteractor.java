5ccafb837b10c3e2ffd3fcc068073827bf552cef
suriananya <suriananya19@gmail.com>
2024-12-03 02:20:41 -0500

    Added mock data to getUserPlaylists, and made sure class extends SpotifyInteractor.

--- SpotifyUserLogin/src/Test/Java/TestRoulette/MockSpotifyInteractor.java
+++ SpotifyUserLogin/src/Test/Java/TestRoulette/MockSpotifyInteractor.java
@@ -1,4 +1,29 @@
 package TestRoulette;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class MockSpotifyInteractor {
+    public JSONObject getUserPlaylists(String userId, int limit, int offset) {
+        JSONObject response = new JSONObject();
+
+        switch (userId) {
+            case "friend1":
+                response.put("items", new JSONArray().put(
+                        new JSONObject().put("id", "playlist1")));
+                break;
+            case "friend2":
+                response.put("items", new JSONArray().put(
+                        new JSONObject().put("id", "playlist2")));
+                break;
+            case "friend3":
+                response.put("items", new JSONArray()); // No playlists
+                break;
+            case "friend4":
+                response.put("items", new JSONArray().put(
+                        new JSONObject().put("id", "playlist3"))); // Playlist exists but no songs
+                break;
+        }
+        return response;
+    }
 }
